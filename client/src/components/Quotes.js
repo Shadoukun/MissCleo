05dee4af1ed3aee3b488b4e181691cb688b185f1
Shadoukun <shadoukun@gmail.com>
2020-05-16 02:30:34 -0400

    PUBLIC_URL fix

--- client/src/components/Quotes.js
+++ client/src/components/Quotes.js
@@ -301,7 +301,7 @@ const QuoteEntry = ({ quote }) => (
     <div className="quoteBody">
       {quote.message}
       {quote.attachments && quote.attachments.map((file, i) =>
-        <img src={`${backendURL}/files/${file}`} alt="" />
+        <img src={`%PUBLIC_URL%/files/${file}`} alt="" />
       )}
     </div>
   </QuoteEntryStyled>
