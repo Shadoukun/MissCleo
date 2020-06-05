0f8efd44a179c9b7ebb4aa5d424a3d5cfa0e579d
Shadoukun <shadoukun@gmail.com>
2020-06-05 19:48:37 -0400

    fancy loading bar for quote page.

--- client/src/pages/Quotes.js
+++ client/src/pages/Quotes.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Box } from '@material-ui/core';
+import { Typography, Box, LinearProgress } from '@material-ui/core';
 import { MemberList } from '../components/QuoteSidebarList';
 import QuoteList from '../components/QuoteList';
 import { QuoteEntry } from '../components/QuoteEntry';
@@ -8,6 +8,18 @@ import ReactPaginate from 'react-paginate';
 import ResponsiveDrawer from '../components/Drawer'
 import { useParams, useLocation } from 'react-router-dom';
 import { backendCall } from '../utilities';
+import Fade from '@material-ui/core/Fade';
+import styled from 'styled-components';
+
+
+const StyledProgress = styled(LinearProgress)`
+${({ theme }) => `
+  background-color: unset;
+
+  .MuiLinearProgress-barColorPrimary {
+    background-color: ${theme.colors.primaryFontColor};
+  }
+`}`
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
@@ -24,13 +36,26 @@ const QuotePage = (props) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [quoteList, setQuoteList] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+  const [update, setUpdate] = useState(1)
   const url = "/quotes"
 
   // Set current page when guild/user changes
   useEffect(() => {
     setCurrentPage(1)
     setSearchString("")
+    setUpdate(update + 1)
   }, [guild, user]);
+
+  const setSearch = (value) => {
+    setSearchString(value)
+    setUpdate(update + 1)
+  }
+
+  const setPage = (value) => {
+    setCurrentPage(value)
+    setUpdate(update + 1)
+  }
 
   // repopulate memberList when guildId changes.
   useEffect(() => {
@@ -48,23 +73,30 @@ const QuotePage = (props) => {
 
   // populate quote list.
   useEffect(() => {
-    (async () => {
-      let params = []
+    setLoading(true)
+    let params = []
+    if (guild) { params.push(`guild=${guild}`) }
+    if (user && !searchString) { params.push(`user=${user}`) }
+    if (searchString) { params.push(`search=${encodeURIComponent(searchString)}`) }
+    if (currentPage) { params.push(`page=${currentPage}`) }
 
-      if (guild) { params.push(`guild=${guild}`) }
-      if (user && !searchString) { params.push(`user=${user}`) }
-      if (searchString) { params.push(`search=${encodeURIComponent(searchString)}`) }
-      if (currentPage) { params.push(`page=${currentPage}`) }
+    backendCall.get(url + `?${params.join('&')}`)
+      .then((result) => {
+        setQuoteList(result.data.quotes)
+        setPageCount(result.data.pages)
 
-      let result = await backendCall.get(url + `?${params.join('&')}`)
-      setQuoteList(result.data.quotes)
-      setPageCount(result.data.pages)
-    })();
+        setTimeout(() => {
+          setLoading(false)
+        }, 250)
+      })
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-  }, [guild, user, searchString, currentPage])
+  }, [update])
 
   return (
     <>
+      <Box style={{ display: loading ? "block" : "none" }}>
+        <StyledProgress color="primary" variant="query" />
+      </Box>
       <ResponsiveDrawer>
         {guild &&
           <MemberList
@@ -75,48 +107,56 @@ const QuotePage = (props) => {
       </ResponsiveDrawer>
 
       <QuoteList>
-        <Box className="quote-list-header" display="flex">
-          {searchString &&
-            <Typography>
-              Search: {searchString}
-            </Typography>
-          }
 
-          {guild &&
-            <QuoteSearch
-              searchString={searchString}
-              setSearchString={setSearchString}
-            />
-          }
-        </Box>
+        <Fade in={!loading} out={loading}>
+          <Box>
+            <Box className="quote-list-header" display="flex">
+              {searchString &&
+                <Typography>
+                  Search: {searchString}
+                </Typography>
+              }
 
-        {quoteList.map((quote, i) =>
-          <QuoteEntry
-            key={i}
-            quote={quote}
-            memberList={memberList}
-          />
-        )}
+              {guild &&
+                <QuoteSearch
+                  searchString={searchString}
+                  setSearchString={setSearch}
+                />
+              }
+            </Box>
 
-        {!!quoteList.length &&
-          <ReactPaginate className="pagination"
-            containerClassName="pagination"
-            breakClassName="page-item"
-            breakLabel={<button className="page-link">...</button>}
-            previousLabel="<"
-            nextLabel=">"
-            pageCount={pageCount}
-            forcePage={currentPage - 1}
-            pageClassName="page-item"
-            previousClassName="page-item"
-            nextClassName="page-item"
-            pageLinkClassName="page-link"
-            previousLinkClassName="page-link"
-            nextLinkClassName="page-link"
-            activeClassName="active"
-            onPageChange={(data) => setCurrentPage(data.selected + 1)}
-          />
-        }
+
+
+            {quoteList.map((quote, i) =>
+              <QuoteEntry
+                key={i}
+                quote={quote}
+                memberList={memberList}
+              />
+            )}
+
+
+            <Box display="flex" m={"auto"}>
+            <ReactPaginate className="pagination"
+              containerClassName="pagination"
+              breakClassName="page-item"
+              breakLabel={<button className="page-link">...</button>}
+              previousLabel="<"
+              nextLabel=">"
+              pageCount={pageCount}
+              forcePage={currentPage - 1}
+              pageClassName="page-item"
+              previousClassName="page-item"
+              nextClassName="page-item"
+              pageLinkClassName="page-link"
+              previousLinkClassName="page-link"
+              nextLinkClassName="page-link"
+              activeClassName="active"
+              onPageChange={(data) => setPage(data.selected + 1)}
+              />
+            </Box>
+          </Box>
+        </Fade>
       </QuoteList>
     </>
   );
