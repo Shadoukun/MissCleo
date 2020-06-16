d0bebe2b83e205340122e4e12f7f5ab765a645e0
Shadoukun <shadoukun@gmail.com>
2020-06-16 16:40:22 -0400

    style user filter listbox

--- client/src/components/page/CommandModal/user-filter.tsx
+++ client/src/components/page/CommandModal/user-filter.tsx
@@ -5,105 +5,52 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import { useTheme, makeStyles } from '@material-ui/core/styles';
 import { VariableSizeList, ListChildComponentProps } from 'react-window';
-import { Typography } from '@material-ui/core';
+import { Typography, Avatar, Box } from '@material-ui/core';
 import { MemberEntry } from '../../../types';
 import { useGuildContext } from '../../../context/Guild';
 import styled from 'styled-components';
 import { FormLabel } from '../../Form';
 import { lighten, darken } from 'polished';
 
-const LISTBOX_PADDING = 8; // px
+const LISTBOX_PADDING = 4; // px
 
 type RenderInputParams = AutocompleteRenderInputParams;
 type ListboxComponentType = React.ComponentType<React.HTMLAttributes<HTMLElement>>
 
 type UserFilterType = string[]
 
-function renderRow(props: ListChildComponentProps) {
-  const { data, index, style } = props;
-  return React.cloneElement(data[index], {
-    style: {
-      ...style,
-      top: (style.top as number) + LISTBOX_PADDING,
-    },
-  });
-}
 
-function useResetCache(data: any) {
-  const ref = React.useRef<VariableSizeList>(null);
-  React.useEffect(() => {
-    if (ref.current != null) {
-      ref.current.resetAfterIndex(0, true);
-    }
-  }, [data]);
-  return ref;
-}
+const StyledVariableListWrapper = styled.div`
+${({ theme }) => `
+  background-color: ${theme.colors.backgroundColor};
+  color: ${theme.colors.primaryFontColor};
 
+`}`
 
-const OuterElementContext = React.createContext({});
+const StyledOption = styled(Box)`
+${({ theme }) => `
+  display: flex;
+  align-items: center;
 
-const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
-  const outerProps = React.useContext(OuterElementContext);
-  return <div ref={ref} {...props} {...outerProps} />;
-});
+  .MuiAvatar-root {
+    height: 32px;
+    width: 32px;
+    margin-right: ${theme.spacing(1)}px;
+  }
 
 
-// Adapter component for react-window
-const ListboxComponent = React.forwardRef<HTMLDivElement>(function ListboxComponent(props, ref) {
-  const { children, ...other } = props;
-  const itemData = React.Children.toArray(children);
-  const theme = useTheme();
-  const smUp = useMediaQuery(theme.breakpoints.up('sm'), { noSsr: true });
-  const itemCount = itemData.length;
-  const itemSize = smUp ? 36 : 48;
+  .display-name {
+    margin-right: ${theme.spacing(1)}px;
+  }
+  .username {
+    font-size: 12px;
+    color: ${darken(0.2, theme.colors.primaryFontColor)};
+  }
 
-  const getChildSize = (child: React.ReactNode) => {
-    if (React.isValidElement(child) && child.type === ListSubheader) {
-      return 48;
-    }
-    return itemSize;
-  };
-
-  const getHeight = () => {
-    if (itemCount > 8) {
-      return 8 * itemSize;
-    }
-    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
-  };
-
-  const gridRef = useResetCache(itemCount);
-
-  return (
-    <div ref={ref}>
-      <OuterElementContext.Provider value={other}>
-        <VariableSizeList
-          itemData={itemData}
-          height={getHeight() + 2 * LISTBOX_PADDING}
-          width="100%"
-          ref={gridRef}
-          outerElementType={OuterElementType}
-          innerElementType="ul"
-          itemSize={(index) => getChildSize(itemData[index])}
-          overscanCount={5}
-          itemCount={itemCount}
-        >
-          {renderRow}
-        </VariableSizeList>
-      </OuterElementContext.Provider>
-    </div>
-  );
-});
-
-
-const useStyles = makeStyles({
-  listbox: {
-    boxSizing: 'border-box',
-    '& ul': {
-      padding: 0,
-      margin: 0,
-    },
-  },
-});
+  .display-name, .username {
+    color: ${theme.colors.primaryFontColor};
+  }
+`}`
 
 const StyledAutocomplete = styled(Autocomplete) <any>`
 ${({ theme }) => `
@@ -127,20 +74,116 @@ ${({ theme }) => `
     }
 
     .MuiAutocomplete-tag {
-    background-color: ${theme.colors.backgroundColor};
-    border-radius: 2px;
-  }
+      color: ${theme.colors.primaryFontColor};
+      background-color: ${theme.colors.secondaryBackground  };
+      border-radius: 4px;
+    }
 
 `}`
+
+const useStyles = makeStyles({
+  listbox: {
+    boxSizing: 'border-box',
+    '& ul': {
+      padding: 0,
+      margin: 0,
+    },
+  },
+});
+
+function renderRow(props: ListChildComponentProps) {
+  const { data, index, style } = props;
+  return React.cloneElement(data[index], {
+    style: {
+      ...style,
+      top: (style.top as number) + LISTBOX_PADDING,
+    },
+  });
+}
+
+function useResetCache(data: any) {
+  const ref = React.useRef<VariableSizeList>(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div className="outer-element" ref={ref} {...props} {...outerProps} />;
+});
+
+
+// Adapter component for react-window
+const ListboxComponent = React.forwardRef<HTMLDivElement>(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData = React.Children.toArray(children);
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), { noSsr: true });
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 48 : 56;
+
+  const getChildSize = (child: React.ReactNode) => {
+    if (React.isValidElement(child) && child.type === ListSubheader) {
+      return 48;
+    }
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <StyledVariableListWrapper ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index: any) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </StyledVariableListWrapper>
+  );
+});
+
 
 const AutocompleteInput = (params: RenderInputParams) => (
   <TextField
     {...params}
-    type="new-password"
+    type="search"
     variant="filled"
     InputProps={{ ...params.InputProps, disableUnderline: true }}
   />
 )
+
+const AutoCompleteOption = ({ member }: { member: MemberEntry }) => (
+  <StyledOption>
+    <Avatar src={member.user.avatar_url} />
+    <Typography className="display-name" noWrap>{member.display_name}</Typography>
+    <Typography className="username" noWrap>{member.user.name}</Typography>
+
+  </StyledOption>
+)
+
 
 type UserFilterInputProps = {
   userFilter: UserFilterType
@@ -150,6 +193,7 @@ type UserFilterInputProps = {
 const VirtualizedUserFilterInput: React.FC<UserFilterInputProps> = ({ userFilter, setUserFilter }) => {
   const classes = useStyles();
 
+  const [open, setOpen] = useState<boolean>(false)
   // array of selected objects
   const [value, setValue] = useState<MemberEntry[]>([]);
   // value of the underlying input element
@@ -200,6 +244,13 @@ const VirtualizedUserFilterInput: React.FC<UserFilterInputProps> = ({ userFilter
   }
 
   const onInputChange = (event: React.ChangeEvent, newInputValue: string) => {
+    // open when the user begins typing
+    // and value isn't empty.
+    if (newInputValue.length > 0) {
+      setOpen(true)
+    } else {
+      setOpen(false)
+    }
     setInputValue(newInputValue)
   }
 
@@ -211,6 +262,8 @@ const VirtualizedUserFilterInput: React.FC<UserFilterInputProps> = ({ userFilter
         <StyledAutocomplete
           id="user-filter-autocomplete"
           multiple
+          open={open}
+          freeSolo
           autoHighlight
           value={value}
           onChange={onChange}
@@ -221,14 +274,10 @@ const VirtualizedUserFilterInput: React.FC<UserFilterInputProps> = ({ userFilter
           classes={classes}
           ListboxComponent={ListboxComponent as ListboxComponentType}
           options={userList}
-          getOptionLabel={(member: MemberEntry) => `${member.display_name} <${member.user.name}>`}
+          getOptionLabel={(member: MemberEntry) => `${member.display_name} (${member.user.name})`}
           getOptionSelected={(member: MemberEntry, value: any) => member.display_name === value.display_name}
           renderInput={AutocompleteInput}
-          renderOption={(member: MemberEntry) => (
-            <Typography noWrap>
-              {member.display_name} - {member.user.name}
-            </Typography>
-          )}
+          renderOption={(member: MemberEntry) => (<AutoCompleteOption member={member} />)}
         />
       </>
     );
